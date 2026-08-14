7463d9b2026f8a09ff630b585a98dbc89d03195f
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 19:50:37 +0300

    Update SQL queries to reference source schema

--- database/source/validation/013_marketing_campaigns_validation.sql
+++ database/source/validation/013_marketing_campaigns_validation.sql
@@ -4,43 +4,43 @@
 
 -- Empty Campaign Names
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE TRIM(campaign_name) = ''
 ;
 
 -- Future Campaign Start Dates
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE start_date > CURRENT_DATE
 ;
 
 -- End dates before start dates
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE end_date < start_date
 ;
 
 -- Negative campaign budgets
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE budget < 0
 ;
 
 -- Negative campaign spend
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE spend < 0
 ;
 
 -- Spend exceeds budget
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE spend > budget
 ;
 
 -- Negative Marketing metrics
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE impressions < 0
    OR clicks < 0
    OR conversions < 0
@@ -48,19 +48,19 @@ WHERE impressions < 0
 
 -- Clicks greater then impressions
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE clicks > impressions
 ;
 
 -- Conversions greater than clicks
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE conversions > clicks
 ;
 
 -- Missing campaign objectives
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE campaign_objective IS NULL
 ;
 
@@ -68,7 +68,7 @@ WHERE campaign_objective IS NULL
 SELECT
     channel,
     COUNT(*) AS campaign_count
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 GROUP BY channel
 ORDER BY campaign_count DESC
 ;
@@ -79,7 +79,7 @@ SELECT
     channel,
     start_date,
     COUNT(*) AS duplicate_count
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 GROUP BY
     campaign_name,
     channel,
@@ -92,12 +92,12 @@ SELECT
     campaign_id,
     campaign_name,
     clicks::DECIMAL / NULLIF(impressions,0) AS click_through_rate
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 ;
 
 -- Campaigns with spend but no activity
 SELECT *
-FROM marketing_campaigns
+FROM source.marketing_campaigns
 WHERE spend > 0
 AND (
     impressions IS NULL
