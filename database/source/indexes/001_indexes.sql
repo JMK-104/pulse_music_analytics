100e5408db6861e7eff3255c2b876891ac98c827
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 19:34:51 +0300

    Update SQL indexes to include 'source' schema

--- database/source/indexes/001_indexes.sql
+++ database/source/indexes/001_indexes.sql
@@ -5,62 +5,62 @@
 
 -- Albums
 CREATE INDEX idx_albums_artist_id
-ON albums(artist_id)
+ON source.albums(artist_id)
 ;
 
 -- Songs
 CREATE INDEX idx_songs_artist_id
-ON songs(artist_id)
+ON source.songs(artist_id)
 ;
 CREATE INDEX idx_songs_album_id
-ON songs(album_id)
+ON source.songs(album_id)
 ;
 
 -- Playlists
 CREATE INDEX idx_playlists_user_id
-ON playlists(user_id)
+ON source.playlists(user_id)
 ;
 
 -- Podcast Episodes
 CREATE INDEX idx_podcast_episodes_podcast_id
-ON podcast_episodes(podcast_id)
+ON source.podcast_episodes(podcast_id)
 ;
 
 -- Sessions
 CREATE INDEX idx_sessions_user_id
-ON sessions(user_id)
+ON source.sessions(user_id)
 ;
 
 -- Listening History
 CREATE INDEX idx_listening_history_user_id
-ON listening_history(user_id)
+ON source.listening_history(user_id)
 ;
 CREATE INDEX idx_listening_history_song_id
-ON listening_history(song_id)
+ON source.listening_history(song_id)
 ;
 CREATE INDEX idx_listening_history_playlist_id
-ON listening_history(playlist_id)
+ON source.listening_history(playlist_id)
 ;
 
 -- Podcast Listening History
 CREATE INDEX idx_podcast_listening_history_user_id
-ON podcast_listening_history(user_id)
+ON source.podcast_listening_history(user_id)
 ;
 CREATE INDEX idx_podcast_listening_history_episode_id
-ON podcast_listening_history(episode_id)
+ON source.podcast_listening_history(episode_id)
 ;
 
 -- Subscriptions
 CREATE INDEX idx_user_subscriptions_user_id
-ON user_subscriptions(user_id)
+ON source.user_subscriptions(user_id)
 ;
 
 -- Payments
 CREATE INDEX idx_payments_user_id
-ON payments(user_id)
+ON source.payments(user_id)
 ;
 CREATE INDEX idx_payments_subscription_id
-ON payments(subscription_id)
+ON source.payments(subscription_id)
 ;
 
 
@@ -71,27 +71,27 @@ ON payments(subscription_id)
 
 -- Listening History
 CREATE INDEX idx_listening_history_playback_timestamp
-ON listening_history(playback_timestamp)
+ON source.listening_history(playback_timestamp)
 ;
 
 -- Podcast Listening History
 CREATE INDEX idx_podcast_listening_history_playback_timestamp
-ON podcast_listening_history(playback_timestamp)
+ON source.podcast_listening_history(playback_timestamp)
 ;
 
 -- Sessions
 CREATE INDEX idx_sessions_start_timestamp
-ON sessions(session_start_timestamp)
+ON source.sessions(session_start_timestamp)
 ;
 
 -- Payments
 CREATE INDEX idx_payments_payment_timestamp
-ON payments(payment_timestamp)
+ON source.payments(payment_timestamp)
 ;
 
 -- Marketing Campaigns
 CREATE INDEX idx_marketing_campaigns_start_date
-ON marketing_campaigns(start_date)
+ON source.marketing_campaigns(start_date)
 ;
 
 
@@ -102,7 +102,7 @@ ON marketing_campaigns(start_date)
 
 -- Listening History: User Activity Over Time
 CREATE INDEX idx_listening_history_user_timestamp
-ON listening_history(
+ON source.listening_history(
     user_id,
     playback_timestamp
 )
@@ -110,7 +110,7 @@ ON listening_history(
 
 -- Listening History: Song Popularity Analysis
 CREATE INDEX idx_listening_history_song_timestamp
-ON listening_history(
+ON source.listening_history(
     song_id,
     playback_timestamp
 )
@@ -118,7 +118,7 @@ ON listening_history(
 
 -- Podcast Listening History: User Engagement
 CREATE INDEX idx_podcast_listening_history_user_timestamp
-ON podcast_listening_history(
+ON source.podcast_listening_history(
     user_id,
     playback_timestamp
 )
@@ -126,7 +126,7 @@ ON podcast_listening_history(
 
 -- Podcast Listening History: Episode Performance
 CREATE INDEX idx_podcast_listening_history_episode_timestamp
-ON podcast_listening_history(
+ON source.podcast_listening_history(
     episode_id,
     playback_timestamp
 )
@@ -134,7 +134,7 @@ ON podcast_listening_history(
 
 -- Payments: Subscription Revenue Analysis
 CREATE INDEX idx_payments_subscription_timestamp
-ON payments(
+ON source.payments(
     subscription_id,
     payment_timestamp
 )
@@ -142,7 +142,7 @@ ON payments(
 
 -- User Subscriptions: Current Subscription Lookup
 CREATE INDEX idx_user_subscriptions_user_status
-ON user_subscriptions(
+ON source.user_subscriptions(
     user_id,
     subscription_status
 )
@@ -150,7 +150,7 @@ ON user_subscriptions(
 
 -- Marketing Campaigns: Campaign Reporting
 CREATE INDEX idx_marketing_campaigns_channel_start_date
-ON marketing_campaigns(
+ON source.marketing_campaigns(
     channel,
     start_date
 )
