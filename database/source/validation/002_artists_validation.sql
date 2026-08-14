c46dcf6ff2ba7a85adab2017261945d00c999458
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 20:06:11 +0300

    Update SQL queries to use source schema

--- database/source/validation/002_artists_validation.sql
+++ database/source/validation/002_artists_validation.sql
@@ -4,16 +4,16 @@
 
 -- Artists with no songs
 SELECT a.*
-FROM artists a
-LEFT JOIN songs s
+FROM source.artists a
+LEFT JOIN source.songs s
     ON a.artist_id = s.artist_id
 WHERE s.artist_id IS NULL
 ;
 
 -- Artists with not albums
 SELECT a.*
-FROM artists a
-LEFT JOIN albums al
+FROM source.artists a
+LEFT JOIN source.albums al
     ON a.artist_id = al.artist_id
 WHERE al.artist_id IS NULL
 ;
@@ -22,8 +22,8 @@ WHERE al.artist_id IS NULL
 SELECT DISTINCT
     a.artist_id,
     a.artist_name
-FROM artists a
-JOIN songs s
+FROM source.artists a
+JOIN source.songs s
     ON a.artist_id = s.artist_id
 WHERE a.is_active = FALSE
   AND s.is_active = TRUE
@@ -33,14 +33,14 @@ WHERE a.is_active = FALSE
 SELECT
     artist_name,
     COUNT(*) AS duplicate_count
-FROM artists
+FROM source.artists
 GROUP BY artist_name
 HAVING COUNT(*) > 1
 ;
 
 -- Missing Country and Genre
 SELECT *
-FROM artists
+FROM source.artists
 WHERE country IS NULL
   AND primary_genre IS NULL
 ;
@@ -49,8 +49,8 @@ WHERE country IS NULL
 SELECT
     a.artist_name,
     COUNT(s.song_id) AS song_count
-FROM artists a
-JOIN songs s
+FROM source.artists a
+JOIN source.songs s
     ON a.artist_id = s.artist_id
 GROUP BY
     a.artist_id,
