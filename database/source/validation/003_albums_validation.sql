858535225a0b8727d1d28e371235b3b30eba6799
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 20:05:04 +0300

    Update SQL queries to use source schema

--- database/source/validation/003_albums_validation.sql
+++ database/source/validation/003_albums_validation.sql
@@ -4,23 +4,23 @@
 
 -- Albums with no songs
 SELECT al.*
-FROM albums al
-LEFT JOIN songs s
+FROM source.albums al
+LEFT JOIN source.songs s
     ON al.album_id = s.album_id
 WHERE s.album_id IS NULL
 ;
 
 -- Albums referencing missing artists
 SELECT al.*
-FROM albums al
-LEFT JOIN artists a
+FROM source.albums al
+LEFT JOIN source.artists a
     ON al.artist_id = a.artist_id
 WHERE a.artist_id IS NULL
 ;
 
 -- Future Release Dates
 SELECT *
-FROM albums
+FROM source.albums
 WHERE release_date > CURRENT_DATE
 ;
 
@@ -30,15 +30,15 @@ SELECT
     al.album_title,
     al.release_date,
     a.created_at AS artist_created_at
-FROM albums al
-JOIN artists a
+FROM source.albums al
+JOIN source.artists a
     ON al.artist_id = a.artist_id
 WHERE al.release_date < a.created_at::DATE
 ;
 
 -- Missing Album Title
 SELECT *
-FROM albums
+FROM source.albums
 WHERE TRIM(album_title) = ''
 ;
 
@@ -47,7 +47,7 @@ SELECT
     artist_id,
     album_title,
     COUNT(*) AS duplicate_count
-FROM albums
+FROM source.albums
 GROUP BY
     artist_id,
     album_title
@@ -60,8 +60,8 @@ SELECT
     s.song_title,
     al.release_date AS album_release_date,
     s.release_date AS song_release_date
-FROM albums al
-JOIN songs s
+FROM source.albums al
+JOIN source.songs s
     ON al.album_id = s.album_id
 WHERE s.release_date < al.release_date
 ;
