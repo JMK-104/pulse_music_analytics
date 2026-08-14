8784b4c068db482394cbd90012e5c922c044dd66
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 18:24:41 +0300

    Add database deployment script for Pulse Music Analytics
    This script outlines the deployment process for the Pulse Music Analytics database, including creating extensions, schemas, and deploying the source database.

---
+++ database/setup/deploy_database.sql
@@ -0,0 +1,60 @@
+-- ==========================================================
+-- Pulse Music Analytics
+-- Database Deployment
+
+-- To execute: Open terminal and first run psql -U postgres -f database/setup/001_create_database.sql
+-- Next, run psql -U postgres -d pulse_music -f database/setup/deploy_database.sql
+
+-- ==========================================================
+
+\echo ''
+\echo '=========================================='
+\echo 'Pulse Music Analytics Database Deployment'
+\echo '=========================================='
+
+
+-- ==========================================================
+-- 1. Extensions
+-- ==========================================================
+
+\echo ''
+\echo 'Creating extensions...'
+
+\ir 002_create_extensions.sql
+
+
+-- ==========================================================
+-- 2. Schemas
+-- ==========================================================
+
+\echo ''
+\echo 'Creating schemas...'
+
+\ir 003_create_schemas.sql
+
+
+-- ==========================================================
+-- 3. Source Database
+-- ==========================================================
+
+\echo ''
+\echo 'Deploying source database...'
+
+\ir ../source/deploy_source_database.sql
+
+
+-- ==========================================================
+-- 4. Analytics Database
+-- ==========================================================
+
+-- To be implemented later.
+
+
+-- ==========================================================
+-- Complete
+-- ==========================================================
+
+\echo ''
+\echo '=========================================='
+\echo 'Database Deployment Complete'
+\echo '=========================================='
