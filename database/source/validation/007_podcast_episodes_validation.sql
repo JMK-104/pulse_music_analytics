e0ea15c178c8dafb801a462e619ed48009bf3d90
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 19:59:30 +0300

    Update SQL queries to use source schema

--- database/source/validation/007_podcast_episodes_validation.sql
+++ database/source/validation/007_podcast_episodes_validation.sql
@@ -4,33 +4,33 @@
 
 -- Episodes with missing podcasts
 SELECT pe.*
-FROM podcast_episodes pe
-LEFT JOIN podcasts p
+FROM source.podcast_episodes pe
+LEFT JOIN source.podcasts p
     ON pe.podcast_id = p.podcast_id
 WHERE p.podcast_id IS NULL
 ;
 
 -- Empty Episode Titles
 SELECT *
-FROM podcast_episodes
+FROM source.podcast_episodes
 WHERE TRIM(episode_title) = ''
 ;
 
 -- Future release dates
 SELECT *
-FROM podcast_episodes
+FROM source.podcast_episodes
 WHERE release_date > CURRENT_DATE
 ;
 
 -- Invalid Episode Durations
 SELECT *
-FROM podcast_episodes
+FROM source.podcast_episodes
 WHERE duration_seconds < 10
 ;
 
 -- Invalid Episode Numbers
 SELECT *
-FROM podcast_episodes
+FROM source.podcast_episodes
 WHERE episode_number <= 0
 ;
 
@@ -39,7 +39,7 @@ SELECT
     podcast_id,
     episode_number,
     COUNT(*) AS episode_count
-FROM podcast_episodes
+FROM source.podcast_episodes
 WHERE episode_number IS NOT NULL
 GROUP BY
     podcast_id,
@@ -53,8 +53,8 @@ SELECT
     pe.release_date,
     p.podcast_title,
     p.created_at::DATE AS podcast_created_date
-FROM podcast_episodes pe
-JOIN podcasts p
+FROM source.podcast_episodes pe
+JOIN source.podcasts p
     ON pe.podcast_id = p.podcast_id
 WHERE pe.release_date < p.created_at::DATE
 ;
@@ -64,8 +64,8 @@ SELECT
     pe.episode_id,
     pe.episode_title,
     p.podcast_title
-FROM podcast_episodes pe
-JOIN podcasts p
+FROM source.podcast_episodes pe
+JOIN source.podcasts p
     ON pe.podcast_id = p.podcast_id
 WHERE pe.is_active = TRUE
 AND p.is_active = FALSE
@@ -75,6 +75,6 @@ AND p.is_active = FALSE
 SELECT
     is_explicit,
     COUNT(*) AS episode_count
-FROM podcast_episodes
+FROM source.podcast_episodes
 GROUP BY is_explicit
 ;
