b5031e7cc3b695e1424d203227ab039e21beef06
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 19:58:21 +0300

    Update SQL queries to use source schema

--- database/source/validation/008_sessions_validation.sql
+++ database/source/validation/008_sessions_validation.sql
@@ -4,40 +4,40 @@
 
 -- Sessions with missing users
 SELECT s.*
-FROM sessions s
-LEFT JOIN users u
+FROM source.sessions s
+LEFT JOIN source.users u
     ON s.user_id = u.user_id
 WHERE u.user_id IS NULL
 ;
 
 -- Session start times in the future
 SELECT *
-FROM sessions
+FROM source.sessions
 WHERE session_start_timestamp > CURRENT_TIMESTAMP
 ;
 
 -- Session end before session start
 SELECT *
-FROM sessions
+FROM source.sessions
 WHERE session_end_timestamp < session_start_timestamp
 ;
 
 -- Extremely Long Sessions
 SELECT *
-FROM sessions
+FROM source.sessions
 WHERE session_end_timestamp - session_start_timestamp 
     > INTERVAL '24 hours'
 ;
 
 -- Sessions with no end timestamp
 SELECT *
-FROM sessions
+FROM source.sessions
 WHERE session_end_timestamp IS NULL
 ;
 
 -- Missing device informaiton
 SELECT *
-FROM sessions
+FROM source.sessions
 WHERE device_type IS NULL
 AND operating_system IS NULL
 ;
@@ -47,8 +47,8 @@ SELECT
     s.session_id,
     s.session_start_timestamp,
     u.user_id
-FROM sessions s
-JOIN users u
+FROM source.sessions s
+JOIN source.users u
     ON s.user_id = u.user_id
 WHERE u.is_active = FALSE
 ;
@@ -58,7 +58,7 @@ SELECT
     user_id,
     DATE(session_start_timestamp) AS session_date,
     COUNT(*) AS session_count
-FROM sessions
+FROM source.sessions
 GROUP BY
     user_id,
     DATE(session_start_timestamp)
