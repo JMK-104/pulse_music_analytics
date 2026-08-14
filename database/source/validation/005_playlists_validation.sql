615d7d8190635adeeeea5e1d581f51c3fd4f5305
Justin Kakuyo <justin.mk.1004@gmail.com>
2026-08-14 20:02:30 +0300

    Change table reference to source.playlists

--- database/source/validation/005_playlists_validation.sql
+++ database/source/validation/005_playlists_validation.sql
@@ -4,21 +4,21 @@
 
 -- User Playlists without owners
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE playlist_type = 'User'
 AND user_id IS NULL
 ;
 
 -- Platform Playlists with owners
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE playlist_type IN ('Editorial', 'Algorithmic')
 AND user_id IS NOT NULL
 ;
 
 -- Invalid Playlist types
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE playlist_type NOT IN
 (
     'User',
@@ -29,19 +29,19 @@ WHERE playlist_type NOT IN
 
 -- Empty Playlist Names
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE TRIM(playlist_name) = ''
 ;
 
 -- Extremely Long Playlists
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE LENGTH(playlist_name) > 200
 ;
 
 -- Public playsts without owners
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE is_public = TRUE
 AND playlist_type = 'User'
 AND user_id IS NULL
@@ -49,7 +49,7 @@ AND user_id IS NULL
 
 -- Recently created inactive playlists
 SELECT *
-FROM playlists
+FROM source.playlists
 WHERE is_public = FALSE
 AND created_at >= CURRENT_TIMESTAMP - INTERVAL '7 days'
 ;
